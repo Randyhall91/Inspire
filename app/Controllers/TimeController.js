43c0b14b3a703d598dfbd417324b23dd408a4d87
Randyhall91 <randyrocks11@gmail.com>
2022-09-17 20:26:40 -0600

    Final

--- app/Controllers/TimeController.js
+++ app/Controllers/TimeController.js
@@ -4,19 +4,15 @@ import { setText } from "../Utils/Writer.js";
 
 
 function getTime() {
-  let hours = new Date().getHours()
+  let hour = new Date().getHours()
   let minutes = new Date().getMinutes()
 
-  let min = ''
-  console.log(minutes);
-
-  if (minutes < 10) {
-    min = ('0' + minutes)
-  }
-
-  if (hours >= 12) {
-    let pm = hours - 12
-    setText('time', (pm + ':' + minutes + 'PM'))
+  if (hour >= 12) {
+    hour = hour - 12
+    hour.toString()
+    setText('time', (hour + ':' + minutes.toString().padStart(2, '0') + ' PM'))
+  } else {
+    setText('time', (hour + ':' + minutes + ' AM'))
   }
 
 }
@@ -25,8 +21,7 @@ function getTime() {
 export class TimeController {
   constructor() {
     getTime()
-
-
+    setInterval(getTime, 1000)
   }
 
 
